b32bb2f5c155a67f3ce61e3fccc799d381b589c5
Kotaro Ito <96063584+Abiti0233@users.noreply.github.com>
2025-04-05 18:34:42 +0900

    feat: memo list screen

--- expo-udemy/src/app/index.tsx
+++ expo-udemy/src/app/index.tsx
@@ -1,11 +1,69 @@
-import { View, Text} from "react-native";
+import { View, Text, StyleSheet } from "react-native";
 
 const index = () => {
   return (
-    <View>
-      <Text>index</Text>
+    <View style={styles.container}>
+      <View>
+        <View>
+          <Text>Memo App</Text>
+          <Text>ログアウト</Text>
+        </View>
+      </View>
+      <View>
+        <View>
+          <View>
+            <Text>買い物リスト</Text>
+            <Text>2025/4/5 10:00</Text>
+          </View>
+          <View>
+            <Text>×</Text>
+          </View>
+        </View>
+
+        <View>
+          <View>
+            <Text>買い物リスト</Text>
+            <Text>2025/4/5 10:00</Text>
+          </View>
+          <View>
+            <Text>×</Text>
+          </View>
+        </View>
+
+        <View>
+          <View>
+            <Text>買い物リスト</Text>
+            <Text>2025/4/5 10:00</Text>
+          </View>
+          <View>
+            <Text>×</Text>
+          </View>
+        </View>
+
+        <View>
+          <View>
+            <Text>買い物リスト</Text>
+            <Text>2025/4/5 10:00</Text>
+          </View>
+          <View>
+            <Text>×</Text>
+          </View>
+        </View>
+      </View>
+
+      <View>
+        <Text>＋</Text>
+      </View>
     </View>
-  )
-}
+  );
+};
 
 export default index;
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+});
