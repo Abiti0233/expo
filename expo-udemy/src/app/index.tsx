05f75674fb7018aec3b0142692504288f6ad2462
Kotaro Ito <96063584+Abiti0233@users.noreply.github.com>
2025-04-05 18:46:50 +0900

    style: header style

--- expo-udemy/src/app/index.tsx
+++ expo-udemy/src/app/index.tsx
@@ -3,13 +3,18 @@ import { View, Text, StyleSheet } from "react-native";
 const index = () => {
   return (
     <View style={styles.container}>
-      <View>
-        <View>
-          <Text>Memo App</Text>
-          <Text>ログアウト</Text>
+      <View style={styles.header}>
+        {/* header */}
+        <View style={styles.headerInner}>
+          <Text style={styles.headerTitle}>Memo App</Text>
+          <Text style={styles.headerRight}>ログアウト</Text>
         </View>
       </View>
+
+      {/* メモ一覧 */}
       <View>
+
+        {/* 個別のメモ */}
         <View>
           <View>
             <Text>買い物リスト</Text>
@@ -63,7 +68,27 @@ export default index;
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: "center",
+    backgroundColor: "white",
+  },
+  header: {
+    backgroundColor: "#467FD3",
+    height: 104,
+    justifyContent: "flex-end",
+  },
+  headerInner: {
     alignItems: "center",
   },
+  headerTitle: {
+    marginBottom: 8,
+    fontSize: 22,
+    lineHeight: 32,
+    fontWeight: "bold",
+    color: "white",
+  },
+  headerRight: {
+    position: "absolute",
+    right: 16,
+    bottom: 16,
+    color: "rgba(255, 255, 255, 0.7)",
+  }
 });
